bbbee9136224ea92d848aeacc9f2299c65e75d8f
Eeshan <eeshan@gmail.com>
2014-10-04 15:05:41 +0530

    BoardHeatMap now uses Loader

--- scripts/BoardHeatMap.R
+++ scripts/BoardHeatMap.R
@@ -10,42 +10,31 @@ source("scripts/ParseFunctions.R")
 source("scripts/BoardFunctions.R")
 
 #Initialize empty board
+sampleFraction <- 0.01
+listdfMoves <- LoadBoards(sampleFraction = sampleFraction, derivative = T)
+
 countBoard <- matrix(data=0, nrow=15, ncol=15)
-
-allFiles <- list.files("data","anno.*\\.gcg")
-
 ctr <- 0
-sampleFraction <- 1 # If we want to use only a fraction of the boards
-for(gcgFileName in sample(allFiles, length(allFiles) * sampleFraction, replace=FALSE)){
-    try(
-    {
-        cat(" ", ctr," ")
-        gcgFileName <- paste("data", gcgFileName, sep ="/")
-        gcg <- readLines(gcgFileName)
-        
-        board <- MakeBoard(gcg)
-        countBoard <- countBoard + as.numeric(board!=".")
-        cat(gcgFileName, "\n") 
-        ctr <- ctr+1
-    })
+for(dfMoves in listdfMoves){
+  board <- MakeBoard(dfMoves)
+  countBoard <- countBoard + as.numeric(board!=".")
+  ctr <- ctr+1
+  cat(" ", ctr, "\n")
 }
 
-(numSuccessful <- countBoard[8,8])
-(numFailed <- ctr - numSuccessful)
-
 countBoard.long <- melt(countBoard, varnames=c("y","x"), value.name="count")
-countBoard.long$PercentGames <- countBoard.long$count*100/numSuccessful
+countBoard.long$PercentGames <- countBoard.long$count*100/length(listdfMoves)
 
 
 #Plot the board on screen
 boardHeatMap <- ggplot(countBoard.long,
                        aes(x = LETTERS[x], y = 16-y, fill = PercentGames)) +
-                       geom_tile()
+  geom_tile()
 boardHeatMap <- boardHeatMap +
-                  coord_equal() +
-                  scale_x_discrete(expand = c(0, 0)) +
-                  scale_y_reverse(breaks=1:15, expand = c(0,0)) +
-                  xlab("") + ylab ("")
+  coord_equal() +
+  scale_x_discrete(expand = c(0, 0)) +
+  scale_y_reverse(breaks=1:15, expand = c(0,0)) +
+  xlab("") + ylab ("")
 
 spectralPalette <- colorRampPalette(rev(brewer.pal(11, "Spectral")), space="Lab")
 boardHeatMap + scale_fill_gradientn(colours = spectralPalette(100))
@@ -54,13 +43,8 @@ boardHeatMap + scale_fill_gradient(low = "#FFFFFF", high = "#00AAAA", space = "L
 
 #Save to file
 jpeg('output/HeatMap1.jpg',width = 860, height = 800)
-  boardHeatMap + scale_fill_gradient(low = "#FFFFFF", high = "#00AAAA", space = "Lab")
+boardHeatMap + scale_fill_gradient(low = "#FFFFFF", high = "#00AAAA", space = "Lab")
 dev.off()
 jpeg('output/HeatMap2.jpg',width = 860, height = 800)
-  boardHeatMap + scale_fill_gradientn(colours = spectralPalette(100))
+boardHeatMap + scale_fill_gradientn(colours = spectralPalette(100))
 dev.off()
-
-
-
-
-
